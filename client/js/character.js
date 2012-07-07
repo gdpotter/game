9c40f1a3906d46bbbf539cee57fff75e1cdd1966
Greg Potter <gdpotter123@gmail.com>
2012-07-07 08:35:59 -0600

    Some minor cleanup and reorganization

--- client/js/character.js
+++ client/js/character.js
@@ -1,5 +1,7 @@
 define(['entity'], function(Entity) {
 
+    var friction = 0.146875 * 10000;
+
     var Characer = Entity.extend({
         init: function() {
             this._super();
@@ -17,6 +19,62 @@ define(['entity'], function(Entity) {
 
             this.hitPoints = 0;
             this.maxHitPoints = 0;
+        },
+
+        update: function(dt) {
+            var tick = 1/dt;
+
+            if (this.movementX == 'left') {
+
+                // If we are currently moving right
+                if (this.velocityX > 0) {
+                    this.velocityX = this.velocityX - (this.deceleration * tick);
+                } else {
+                    this.velocityX = Math.max(this.maxVelocity * -1, (this.velocityX - (this.acceleration * tick)));
+                }
+                this.state = 'run';
+
+                this.direction = 'left';
+
+            } else if (this.movementX == 'right') {
+
+                // If we are currently moving left
+                if (this.velocityX < 0) {
+                    this.velocityX = this.velocityX + (this.deceleration * tick);
+                } else {
+                    this.velocityX = Math.min(this.maxVelocity, (this.velocityX + (this.acceleration * tick)));
+                }
+
+                this.state = 'run';
+
+                this.direction = 'right';
+
+            } else {
+                if (this.velocityX < 0) {
+                    this.velocityX = Math.min(this.velocityX + (friction * tick), 0);
+
+                } else if (this.velocityX > 0) {
+                    this.velocityX = Math.max(this.velocityX - (friction * tick), 0);
+                } else {
+                    this.state = 'idle';
+                }
+            }
+
+            if (this.jumping && this.velocityY === 0) {
+                this.velocityY = -750;
+            }
+
+            if (this.jumping) {
+                this.state = 'jump';
+            }
+
+            // Gravity
+            this.velocityY = this.velocityY + (this.gravity * tick);
+
+            this.setAnimation(this.state + '_' + (this.direction || 'right'));
+
+            this.x = this.x + this.velocityX * tick;
+            this.y = this.y + this.velocityY * tick;
         }
     });
 
--- client/js/updater.js
+++ client/js/updater.js
@@ -1,7 +1,5 @@
 define(function() {
 
-    var friction = 0.146875 * 10000;
-
     var Updater = Class.extend({
         init: function(game) {
             this.game = game;
@@ -34,69 +32,8 @@ define(function() {
         updateCharacters: function() {
             var self = this;
 
-            this.updateCharacter(this.game.player);
+            this.game.player.update(this.game.renderer.realFPS);
             this.detectCollisions(this.game.player);
-        },
-
-        updateCharacter: function(c) {
-            var self = this;
-
-            var tick = 1/this.game.renderer.realFPS;
-
-            if (c.movementX == 'left') {
-
-                // If we are currently moving right
-                if (c.velocityX > 0) {
-                    c.velocityX = c.velocityX - (c.deceleration * tick);
-                } else {
-                    c.velocityX = Math.max(c.maxVelocity * -1, (c.velocityX - (c.acceleration * tick)));
-                }
-                c.state = 'run';
-                // c.setAnimation('run_left');
-            } else if (c.movementX == 'right') {
-
-                // If we are currently moving left
-                if (c.velocityX < 0) {
-                    c.velocityX = c.velocityX + (c.deceleration * tick);
-                } else {
-                    c.velocityX = Math.min(c.maxVelocity, (c.velocityX + (c.acceleration * tick)));
-                }
-
-                c.state = 'run';
-                // c.setAnimation('run_right');
-            } else {
-                if (c.velocityX < 0) {
-                    c.velocityX = Math.min(c.velocityX + (friction * tick), 0);
-
-                    // c.setAnimation('idle_left');
-                } else if (c.velocityX > 0) {
-                    c.velocityX = Math.max(c.velocityX - (friction * tick), 0);
-                } else {
-                    c.state = 'idle';
-                }
-
-                if (c.velocityX > 0) {
-                    // c.setAnimation('idle_right');
-                }
-            }
-
-            if (c.jumping && c.velocityY === 0) {
-                c.velocityY = -750;
-            }
-
-            if (c.jumping) {
-                c.state = 'jump';
-            }
-
-            c.setAnimation(c.state + '_' + (c.movementX || 'right'));
-
-            // if (c.jumping) {
-                c.velocityY = c.velocityY + (c.gravity * tick);
-            // }
-
-            c.x = c.x + c.velocityX * tick;
-            c.y = c.y + c.velocityY * tick;
-
         },
 
         detectCollisions: function(c) {
@@ -112,11 +49,6 @@ define(function() {
             var b2x = Math.floor((bb.x + bb.w - 1) / 24); // Second below block position
             var by = Math.floor((bb.y + bb.h) / 24);
 
-            // this.game.renderer.context.fillStyle = "blue";
-            // this.game.renderer.context.fillRect(b1x * 24, by * 24, 24, 24);
-            // this.game.renderer.context.fillRect(b2x * 24, by * 24, 24, 24);
-            // console.log(this.game.map.getTile(b1x, by));
-
             if (this.game.player.velocityY >= 0) {
                 if (this.game.map.isSolid(b1x, by) || this.game.map.isSolid(b2x, by)) {
                     this.game.player.velocityY = 0;
@@ -125,22 +57,7 @@ define(function() {
                 }
             }
 
-            /*if (this.checkCollision(bb.x, bb.y, bb.w, bb.h, b1x, by, 24, 24)) {
-                this.game.player.velocityY = 0;
-                this.game.player.jumping = false;
-            }*/
         },
-
-        /*checkCollision: function(ax1, ay1, aw, ah, bx1, by1, bw, bh) {
-            var ax2 = ax1 + aw,
-                ay2 = ay1 + ah,
-                bx2 = bx1 + bw,
-                by2 = by1 + bh;
-
-                // console.log(ax1 < bx2, ax2 > bx1 , ay1 < by2 , ay2 > by1)
-
-              return ax1 < bx2 && ax2 > bx1 && ay1 < by2 && ay2 > by1;
-        },*/
 
         updateAnimations: function() {
 
